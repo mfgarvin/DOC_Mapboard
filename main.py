3cde469a9a66286243dbdce4ba773f9623348907
Michael Garvin <mfjgarvin@gmail.com>
2022-02-16 18:58:59 -0500

    Added functioning chronos(), basic time logic.

--- main.py
+++ main.py
@@ -2,12 +2,16 @@
 
 #Things we need to import go here:
 import json
+import datetime
+import time
 
 #Variables we need to set go here:
 JSON_LOCATION="./demo.json"
 LED_ALLOCATION="./leds.txt"
 PARISH_ID="./ids.txt"
-WEEKDAYS=["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+DAYS=["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+WEEKDAYS=["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+WEEKEND=["Saturday", "Sunday"]
 #Functions? *Raises hand*
 def ingest():
 	readfile = open(JSON_LOCATION, "r")
@@ -26,17 +30,21 @@ def digest():
 	adoration_database = {}
 	try:
 		for parish in allocation.copy():
-			for day in WEEKDAYS: # Mass Times
+#								parishName = parish[0]
+			for day in DAYS: # Mass Times
 				db1[day] = rawjson[parish[0]]['Mass Times'][day]
 			masstime_database[parish[0]] = db1
-			for day in WEEKDAYS: # Confession Times
+			db1 = {}
+			for day in DAYS: # Confession Times
 				db2[day] = rawjson[parish[0]]['Confessions'][day]
 			confession_database[parish[0]] = db2
-			WEEKDAYS.append("is24hr")
-			for day in WEEKDAYS: # Adoration Times
+			db2 = {}
+			DAYS.append("is24hr")
+			for day in DAYS: # Adoration Times
 				db3[day] = rawjson[parish[0]]['Eucharistic Adoration'][day]
 			adoration_database[parish[0]] = db3
-			WEEKDAYS.remove("is24hr")
+			db3 = {}
+			DAYS.remove("is24hr")
 	except KeyError as e:
 		if str(e) == "0":
 			print("Out of parishes, continuing...")
@@ -44,11 +52,7 @@ def digest():
 		else:
 			raise
 	finally:
-		print("Database entries:")
-		print("\nMasses:", masstime_database)
-		print("\nConfessions:", confession_database)
-		print("\nAdoration:", adoration_database)
-			#more to come later
+		pass
 
 #Combining Parish Name, ID, and LED Allocation into one dictionary
 def setID():
@@ -61,11 +65,57 @@ def setID():
 	for key, id in iddict.items(): 		# key = parish name
 		allocation[id - 1][0] = key
 		allocation[id - 1][1] = id
-		allocation[id - 1][2] = leddict.get(key)
+		allocation[id - 1][2] = leddict.get(key) #LED Assignment
+
+def chronos():
+	#Take an ID from the allocation variable, check it against the different databses. - DONE
+	#must be specific by weekday - DONE
+	#If scheduled to be active, call the display() function. - DONE
+	#format should be something like: ID, State/color, duration. - DONE
+	#Add Logic to priortize status (probably should just use ifelse statements). - DONE
+	#will need to either be called every x minites, or run on a loop.
+
+	currentTime = datetime.datetime.now()
+	hTime = 900
+	if hTime != currentTime.strftime("%-H:%M"):
+		print("Time Not Accurate - Testing Enabled")
+	weekday = currentTime.strftime("%A")
+	if weekday != currentTime.strftime("%A"):
+		print("Day Not Accurate - Testing Enabled")
+	print(weekday, hTime)
+	#Defining Duration
+	if weekday in WEEKDAYS:
+		liturgyDuration = 30
+	if weekday in WEEKEND:
+		liturgyDuration = 60
+	try:
+		for parish in allocation.copy():
+			if hTime == masstime_database[parish[0]][weekday]:  #Check to see if they're having Mass
+				display(parish[1], "mass", liturgyDuration)
+			elif hTime == adoration_database[parish[0]][weekday]: #Check to see if Adoration is occuring
+				display(parish[1], "adoration", "TBD")
+			elif hTime == confession_database[parish[0]][weekday]: #Check to see if Confessions are being heard
+				display(parish[1], "confession", "TBD")
+	except KeyError as e:
+		if str(e) == "0":
+			print("Cycle Finished")
+			pass
+		else:
+			raise
+
+def display(id, state, duration):
+	#This will be called by chronos individually for each ID.
+	#Must find LED assignment from key/id
+	#Assign a LED its color, and store its duration & start time in variables.
+	#When time is up, turn off the led.
+	#Add logic to include some sort of a slow, randomized pulsing, so it's not just a static led display
+	#Read the actual LED status, determine whether or not to clear it before assigning a new status.
+	print(id, state, duration)
 
 setID()
 ingest()
 digest()
+chronos()
 
 
 '''
