29ea102249dbda4faaf8fc3ee9e90b2a540f7a8f
Michael Garvin <mfjgarvin@gmail.com>
2022-05-05 15:08:39 -0400

    Simple Calibration Tool for Identifying LEDs

---
+++ calibrate.py
@@ -0,0 +1,17 @@
+import board
+import neopixel
+
+#How many LEDs are connected to the system?
+ledCount = 400
+
+pixels = neopixel.NeoPixel(board.D18, ledCount)
+
+for n in range(ledCount):
+	print("\n")
+	print("LED Number:", n)
+	pixels[n-1] = (0, 0, 0)
+	pixels[n] = (255, 255, 255)
+	input("Press Enter to advance")
+
+print("Calibration Complete. Please populate leds.txt appropritely.")
+pixels.fill((0, 0, 0))
