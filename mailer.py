3fa2d59281b257963a81fca0d4ba8592ba9894e7
Michael Garvin <mfjgarvin@gmail.com>
2022-08-11 10:17:02 -0400

    Added some basic email notifications (for errors, debugging, crashes, etc.)

--- mailer.py
+++ mailer.py
@@ -9,13 +9,3 @@ yag = yagmail.SMTP(username, password)
 
 def sendmail(subject, body):
 	yag.send(to, subject, body)
-#subject = "Hello there!"
-
-#contents = ['Hey there! \n This is the body of my email, and here is the text. ']
-
-#yag.send(to, subject, contents)
-
-#yag = yagmail.SMTP('mygmailusername', 'mygmailpassword')
-#contents = ['This is the body, and here is just text http://somedomain/image.png',
-#            'You can find an audio file attached.', '/local/path/song.mp3']
-#yag.send('to@someone.com', 'subject', contents)
